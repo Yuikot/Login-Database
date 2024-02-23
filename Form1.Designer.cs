d5583ec555c822a14fb01a7bf1e8efb428d2b806
Yuikot <Adrianna.ryza1@wp.pl>
2024-02-23 18:40:43 +0100

    a

---
+++ Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿using System.Drawing;
+
+namespace Login
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.login_label = new System.Windows.Forms.Label();
+            this.password_label = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.login_button = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // login_label
+            // 
+            this.login_label.AutoSize = true;
+            this.login_label.Font = new System.Drawing.Font("Segoe UI", 20.25F);
+            this.login_label.Location = new System.Drawing.Point(115, 101);
+            this.login_label.Name = "login_label";
+            this.login_label.Size = new System.Drawing.Size(78, 37);
+            this.login_label.TabIndex = 0;
+            this.login_label.Text = "login";
+            // 
+            // password_label
+            // 
+            this.password_label.AutoSize = true;
+            this.password_label.Font = new System.Drawing.Font("Segoe UI", 20F);
+            this.password_label.Location = new System.Drawing.Point(115, 217);
+            this.password_label.Name = "password_label";
+            this.password_label.Size = new System.Drawing.Size(130, 37);
+            this.password_label.TabIndex = 1;
+            this.password_label.Text = "password";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.textBox1.Location = new System.Drawing.Point(299, 102);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(170, 38);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // login_button
+            // 
+            this.login_button.AutoSize = true;
+            this.login_button.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.login_button.Location = new System.Drawing.Point(336, 286);
+            this.login_button.Name = "login_button";
+            this.login_button.Size = new System.Drawing.Size(98, 31);
+            this.login_button.TabIndex = 3;
+            this.login_button.Text = "login";
+            this.login_button.UseVisualStyleBackColor = true;
+            this.login_button.Click += new System.EventHandler(this.login_button_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 20F);
+            this.textBox2.Location = new System.Drawing.Point(299, 211);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(170, 43);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.label1.Location = new System.Drawing.Point(12, 348);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(599, 21);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "status";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 390);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.login_button);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.password_label);
+            this.Controls.Add(this.login_label);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button login_button;
+        private System.Windows.Forms.Label login_label;
+        private System.Windows.Forms.Label password_label;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySqlConnector;
+
+namespace Login
+{
+    public partial class Form1 : Form
+    {
+        string login;
+        string password;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Login";
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            label1.Text = "";
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            login = textBox1.Text;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            password = textBox2.Text;
+            textBox2.UseSystemPasswordChar = true;
+        }
+        private void login_button_Click(object sender, EventArgs e)
+        {
+            RunQuery();
+        }
+
+        void RunQuery()
+        {
+            string cs = @"server=localhost;userid=testing;password=Psik123;database=users"; 
+            using (var con = new MySqlConnection(cs))
+            {
+                con.Open();
+                string query = "SELECT COUNT(*) FROM login_password WHERE login = @login AND password = @password";
+                using (var cmd = new MySqlCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@login", login);
+                    cmd.Parameters.AddWithValue("@password", password);
+                    int count = Convert.ToInt32(cmd.ExecuteScalar());
+                    if (count > 0)
+                    {
+                        label1.ForeColor = Color.Green;
+                        label1.Text = "You have successfully logged into your account";
+                    }
+                    else
+                    {
+                        label1.ForeColor = Color.Red;
+                        label1.Text = "Wrong password or login. Try again";
+                    }
+                }
+            }
+        }
+
+    }
+}
